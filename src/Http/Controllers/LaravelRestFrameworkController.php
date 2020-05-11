44ec502317dfdbc1851675a13e6da117ceb61785
Khuram Javed <m.khuramj@gmail.com>
2020-05-12 02:50:29 +0500

    Base controller for the Laravel Rest Frameworks

--- src/Http/Controllers/LaravelRestFrameworkController.php
+++ src/Http/Controllers/LaravelRestFrameworkController.php
@@ -10,79 +10,104 @@
 
 namespace LaravelRestFramework\Http\Controllers;
 
+use App\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Exceptions\HttpResponseException;
-
-use LaravelRestFramework\Http\Api;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
+use LaravelRestFramework\Http\Controllers\Api;
 use LaravelRestFramework\Services\HttpStatus;
 use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Auth\User as Authenticatable
 use ReflectionException;
 
 class LaravelRestFrameworkController extends Controller
 {
 
     /**
-     * The Laravel application instance.
+     * The application implementation.
      *
      * @var \Illuminate\Contracts\Foundation\Application
      */
     protected $app;
 
     /**
-     * The request, of the "Illuminate\Http\Request", created and injected by Laravel.
+     * The request
+     *
+     * @var Illuminate\Http\Request
      */
     public $request;
 
     /**
-     * The name of the model, in lower, binded to the controller
+     * The name of the model in lower case
+     *
+     * @var string
      */
     public $model;
 
     /**
      * The fully qualified model class
+     *
+     * @var \Illumiate\Database\Eloquent\Model
      */
     public $modelClass;
 
     /**
-     * FormRequests will get registered in the controller to get used by the CRUD Traits.
+     * ApiRequests will get registered in the controller and will used by the CRUD Traits.
+     * 
+     * @var array [LaravelRestFramework\Http\Requests\ApiRequest]
      */
     public $apiRequests;
 
     /**
      * The current action, out of ["create", "update", "list", "retrieve", "delete"], the user want to perfrom
+     * 
+     * @var string
      */
     public $action;
 
     /**
-     * Object of type Illuminate\Database\Eloquent\Builder to hold the querySet (QueryBuilder)
+     * The querySet for the model binded with the Countroller
+     *
+     * @var Illuminate\Database\Eloquent\Builder
      */
     public $querySet;
 
     /**
      * The field to lookup while getting object using getObject.
+     *
+     * @var str
      */
     public $lookupField = 'id';
 
     /**
      * The id of the instance currently under the process during the request.
+     *
+     * @var str|int
      */
     public $instanceId;
 
     /**
      * Holds the permissions to check the validaty of the action requested in the current request.
+     *
+     * @var array [LaravelRestFramework\Http\Permissions\BasePermission]
      */
     public $permissions = [];
 
     /**
      * Holds the permissions to check the validaty of the action requested in the current request.
+     *
+     * @var LaravelRestFramework\Http\Filters\BaseFilter
      */
     public $filterClass;
 
     /**
      * The resource based on the action , out of ["create", "update", "list", "retrieve", "delete"],
      * the user want to perfrom. Collection Resource for the "list" action.
+     * 
+     * @var array
      */
     public $modelResource;
 
@@ -91,23 +116,57 @@ class LaravelRestFrameworkController extends Controller
         $this->app = app();
         if ($this->app->runningInConsole())
             return TRUE;
+
+        $this->request = $request;
+    }
+
+    /**
+     * Execute an action on the controller.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function callAction($method, $parameters)
+    {
+        $this->init($this->request);
+        return parent::callAction($method, $parameters);
+    }
+
+    /**
+     * Sets the necessary things to be used for the permissions and filters.
+     *
+     * @param Illuminate\Http\Request $request
+     */
+    public function init(Request $request): void
+    {
+        $user = $this->getLoggedInUser();
+        try {
+            $request->user = User::with('permissions')
+                                 ->find($user->id);
+        } catch (RelationNotFoundException $exc) {
+            $request->user = User::find($user->id);
+            $request->user->permissions = [];
+        }
+        
         $this->request = $request;
         $this->action = $request->route()->getActionMethod();
         $this->modelResource = $this->getResourceClass();
         $this->modelClass = $this->getModelClass();
         $this->instanceId = $this->request->route($this->model);
         $this->permissions = $this->getPermissions();
+
         $this->checkPermissions($this->request);
     }
 
     /**
-     * Given a queryset, filter it with whichever filter backend is in use.
-     * You are unlikely to want to override this method, although you may need
-     * to call it either from a list view, or from a custom `get_object`
-     * method if you want to apply the configured filtering backend to the
-     * default querySet
+     * Given a queryset, filter it with whichever filter backend is in use. You are unlikely to want to
+     * override this method, although you may need to call it either from a list view, or from a custom
+     * `get_object` method if you want to apply the configured filtering backend to the default querySet
+     * 
+     * @param Illuminate\Database\Eloquent\Builder $querySet
      */
-    public function filterQuerySet($querySet)
+    public function filterQuerySet(Builder $querySet): Builder
     {
         if (!$this->filterClass)
             return $querySet;
@@ -117,7 +176,10 @@ class LaravelRestFrameworkController extends Controller
         return $querySet;
     }
 
-    public function getQuerySet()
+    /**
+     * querySet provider
+     */
+    public function getQuerySet(): void
     {
         $querySet = $this->querySet;
         if (!$querySet) {
@@ -184,6 +246,9 @@ class LaravelRestFrameworkController extends Controller
         return $object;
     }
 
+    /**
+     * get the requested object or throw ModelNotFoundException exception
+     */
     public function getObjectOr404($querySet)
     {
         try {
@@ -209,16 +274,30 @@ class LaravelRestFrameworkController extends Controller
         $resource = 'Resource';
         if ($this->action === 'list')
             $resource = 'Collection';
-        return $this->getRootNameSpace() . "Http\\Resources\\" . ucfirst($this->model) . $resource;
+        return $this->getRootNameSpace() . ucfirst($this->model) . $resource;
     }
 
-    public function getModelClass()
+    /**
+     * Get qualified Model Calss
+     */
+    public function getModelClass(): string
     {
         return  $this->getRootNameSpace() . ucfirst($this->model);
     }
 
-    public function getRootNameSpace()
+    /**
+     * get the root name space
+     */
+    public function getRootNameSpace(): string
     {
         return $this->app->getNamespace();
+    }
+
+    /**
+     * get the currently authenticated/loggedin user
+     */
+    public function getLoggedInUser(): Authenticable
+    {
+        return auth()->user();
     }
 }
